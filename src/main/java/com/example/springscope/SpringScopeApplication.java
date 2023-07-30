2e394999a025294fee520b2e73e9c1c8872f8400
mikha <Mikhailov.dzenphone@gmail.com>
2023-07-30 11:16:53 +0300

    Initial commit

---
+++ src/main/java/com/example/springscope/SpringScopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScopeApplication.class, args);
+    }
+
+}
